1976da726bbbe526027d09b832c774829673c841
Shivya <shivyagarg3@gmail.com>
2023-04-08 03:29:06 +0530

    '2'

---
+++ SUB 1_ SESSION 4/BandPowerCalculation.m
@@ -0,0 +1,16 @@
+%initialize
+bandlow = 4;
+bandhigh = 7;
+powarray=zeros(1,EEG.nbchan);
+%loop through all channels
+for chani = 1:EEG.nbchan
+    %channel number of your specific lead
+    data = EEG.data(chani,:);
+    [power, freq] = spectopo(data, 0, EEG.srate,'plot','off');
+    %computing power spectrum in dB
+    bandIdx = find(freq == bandlow) : find(freq == bandhigh);
+    %average power of EEG band in dB
+    temp = 10^(mean(power(bandIdx))/10);
+    powarray(chani) = temp;
+end
+meanabspow = mean(powarray);
---
+++ SUB 1_ SESSION 4/PlottingBandPower.m
@@ -0,0 +1,15 @@
+% Plot the band power as topomap
+figure(1), clf; colormap jet 
+subplot(1,4,1)
+topoplot(powarray,EEG.chanlocs,'numcontour',0,'electrodes','on','shading','interp');
+title('\color{red}\fontsize{13} Band power Topomaps')
+axis square
+set(gca, 'clim',[-11 11],'Position',[0.1 0.008 0.3 1])
+colorbar('Location','south','Position',[0.16 0.12 0.2 0.02],'Box','off')
+% plot the band power as bar graph
+subplot(1,4,[3,4])
+bar(powarray,'r','LineWidth',0.2,'LineStyle','none')
+set(gca,'Box','off','XTickLabel',{EEG.chanlocs.labels},'Ylim',[0 (max(powarray)+1)])
+xlabel('Channels'), ylabel('Absolute power')
+axis square
+title('\color{red}\fontsize{13} Absolute band power at each channel')
